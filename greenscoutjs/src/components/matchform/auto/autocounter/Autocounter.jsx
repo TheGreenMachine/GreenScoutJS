9bbaa01d06480c95b2cb9413831a7ca94d6a1870
Awesome65432139 <104233315+AWESOME654321@users.noreply.github.com>
2026-03-20 11:59:32 -0400

    Auto counter now text field

--- greenscoutjs/src/components/matchform/auto/autocounter/Autocounter.jsx
+++ greenscoutjs/src/components/matchform/auto/autocounter/Autocounter.jsx
@@ -1,39 +1,17 @@
 import "./Autocounter.css";
 
 function Autocounter({ nameText, value, onChange, name }) {
-  const subtract = (event) => {
-    event.preventDefault();
-    if (value > 0) {
-      onChange({
-        target: {
-          name: name,
-          value: value - 1,
-        },
-      });
-    }
-  };
-
-  const add = (event) => {
-    event.preventDefault();
-    onChange({
-      target: {
-        name: name,
-        value: value + 1,
-      },
-    });
-  };
-
   return (
     <div className="child textAutoCounter">
       <p id="autocounttext" className="textAutoCounter">
         {nameText}
       </p>
-      <button onClick={subtract} className="textAutoCounter" id="remove">
-        -
-      </button>
-      <button onClick={add} className="textAutoCounter" id="addto">
-        {value}
-      </button>
+      <textarea
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="notesbox"
+      ></textarea>
     </div>
   );
 }
