87b3f6b5697af8a09b5f24daf3377862ccce107d
Dog43y <71520770+DogeDoge17@users.noreply.github.com>
2026-04-10 09:13:38 -0500

    Load all themes at once

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,47 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path, { basename } from "path";
-import { log } from "console";
+import path from "path";
+import fs from "fs";
+
+function generateThemeLinks() {
+  const relativePath = "public/themes";
+  const dirPath = path.resolve(__dirname, relativePath);
+  const files = fs.readdirSync(dirPath);
+
+  let themeLinks = [];
+  let themeNames = [];
+  for (let i = 0; i < files.length; ++i) {
+    let index = files[i].lastIndexOf(".css");
+    if (index === -1) continue;
+    themeNames.push(files[i].substring(0, index));
+
+    const thePath = path.resolve(relativePath, files[i]);
+    const link = `<link href="${thePath}" rel="stylesheet" id="themeLink">`;
+    themeLinks.push(link);
+  }
+  return { themeLinks, themeNames };
+}
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  // Backend URL is defined in .env.production
-  const useBackend = env.VITE_BACKEND_URL !== "";
+  const useBackend = env.VITE_BACKEND_URL !== ""; // Backend URL is defined in .env.production
+
+  const { themeLinks, themeNames } = generateThemeLinks();
 
   return {
     css: {
       transformer: "lightningcss",
+    },
+    define: {
+      __THEME_NAMES__: JSON.stringify(themeNames),
     },
     plugins: [
       react(),
       {
         name: "inject-html",
         transformIndexHtml(html) {
-          return html.replace(
-            "<THEMEANCHOR/>",
-            `<link href="${env.VITE_BACKEND_URL}/getTheme" rel="stylesheet" id="themeLink">`,
-          );
+          return html.replace("<THEMEANCHOR/>", themeLinks.join("\n"));
         },
       },
     ],
